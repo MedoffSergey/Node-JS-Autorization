f4b97ca1822b0351d6594fe72947fbe11d103bc0
Ваше Имя <you@example.com>
2019-08-27 00:05:29 +0300

    работа с дома

--- src/public/script.js
+++ src/public/script.js
@@ -1,120 +1,129 @@
 'use strict'
 
 
-function delete_files (value) {
+function delete_files(value) {
 
-    console.log('Из текущей директории был удален файл '+ value );
-    let va =  document.getElementById(value)
-    va.remove();
-    $.ajax({
-      url: "/delete",   //путь
-      type: "GET",    //Метод отправки
-      data:{          //передается ключ значение после ?
-        id:value
-      },
-      success: function(){
-        window.location.reload()    //если успешно, то перезапускаем страницу через аякс
-      }
+  console.log('Из текущей директории был удален файл ' + value);
+  let va = document.getElementById(value)
+  va.remove();
+  $.ajax({
+    url: "/delete", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
+      id: value
+    },
+    success: function() {
+      window.location.reload() //если успешно, то перезапускаем страницу через аякс
+    }
 
-    });
+  });
 
 }
 
-function add_files () {
-    let result = document.getElementById ('recipient-name').value ;
-    console.log(result)
+function add_files() {
+  let result = document.getElementById('recipient-name').value;
+  console.log(result)
 
-    let arr = document.getElementById ('recipient-ip').value;
-    console.log(arr)
-
-    $.ajax({
-      url: "/add",   //путь
-      type: "GET",  //Метод отправки
-      data:{        //передается ключ значение после ?
-        domain: result,
-        ip: arr
-      },
-      success: function(){
-        window.location.reload()    //если успешно, то перезапускаем страницу через аякс
-      }
-    });
-}
-
-function superSearch(){
-  let searchResult = document.getElementById ('searchInput').value;
-
+  let arr = document.getElementById('recipient-ip').value;
+  console.log(arr)
 
   $.ajax({
-    url: "/search",   //путь
-    type: "GET",  //Метод отправки
-    data:{        //передается ключ значение после ?
-      search: searchResult,
-
+    url: "/add", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
+      domain: result,
+      ip: arr
     },
-    success: function(){
-      window.location.reload()    //если успешно, то перезапускаем страницу через аякс
+    success: function() {
+      window.location.reload() //если успешно, то перезапускаем страницу через аякс
     }
   });
 }
 
-function loginAsUser(){
-  let loginResult = document.getElementById ('inputLogin').value;
-  let passwordResult = document.getElementById ('inputPassword').value;
+function superSearch() {
+  let searchResult = document.getElementById('searchInput').value;
+
 
   $.ajax({
-    url: "/getLogin",   //путь
-    type: "GET",  //Метод отправки
-    data:{        //передается ключ значение после ?
+    url: "/search", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
+      search: searchResult,
+
+    },
+    success: function() {
+      window.location.reload() //если успешно, то перезапускаем страницу через аякс
+    }
+  });
+}
+
+function loginAsUser() {
+  let loginResult = document.getElementById('inputLogin').value;
+  let passwordResult = document.getElementById('inputPassword').value;
+
+  $.ajax({
+    url: "/getLogin", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
       login: loginResult,
-      password:passwordResult
+      password: passwordResult
     },
 
-    success: function(out){
-          if (out.status == 1){
-          document.getElementById("idLi").innerHTML=out.user; //вывод html
-          document.getElementById("nameLi").innerHTML=out.name;
+    success: function(out) {
+      if (out.status == 1) {
+        document.getElementById("idLi").innerHTML = out.user; //вывод html
+        document.getElementById("nameLi").innerHTML = out.name;
 
-        }
-        else{
-          alert("Пользователь не найден")
-        }
-  }
-});
+      } else {
+        alert("Пользователь не найден")
+      }
+    }
+  });
 }
 
 
-function loginAsAdmin(){
-  let adminLogin = document.getElementById ('inputLogin').value;
-  let adminPassword = document.getElementById ('inputPassword').value;
+function loginAsAdmin() {
+  let adminLogin = document.getElementById('inputLogin').value;
+  let adminPassword = document.getElementById('inputPassword').value;
   $.ajax({
-    url: "/admin",   //путь
-    type: "GET",  //Метод отправки
-    data:{        //передается ключ значение после ?
+    url: "/admin", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
       login: adminLogin,
       password: adminPassword
 
     },
 
-    success: function(userList){
-           window.location.href = 'admin';
-        }
-       });
-     }
+    success: function(userList) {
+      window.location.href = 'admin';
+    }
+  });
+}
 
-function addNewUser(){
-  let newUserName = document.getElementById ('addInputName').value;
-  let newUserLogin = document.getElementById ('addInputLogin').value;
-  let newUserPassword = document.getElementById ('addInputPassword').value;
+function authentication(userList, login) {
+    for (let i = 0; i < userList.length; i++) {
+        if (userList[i].login === login) {
+            return userList[i]
+        }
+    }
+    return false
+}
+
+function addNewUser() {
+  let newUserName = document.getElementById('addInputName').value;
+  let newUserLogin = document.getElementById('addInputLogin').value;
+  let newUserPassword = document.getElementById('addInputPassword').value;
+
   $.ajax({
-    url: "/ajax/admin",   //путь
-    type: "GET",  //Метод отправки
-    data:{        //передается ключ значение после ?
+    url: "/ajax/admin", //путь
+    type: "GET", //Метод отправки
+    data: { //передается ключ значение после ?
       name: newUserName,
       login: newUserLogin,
       password: newUserPassword
     },
-    success: function(){
-           window.location.reload();
-        }
-       });
-     }
+    success: function() {
+      window.location.reload();
+    }
+  });
+}
--- src/server.js
+++ src/server.js
@@ -1,158 +1,175 @@
   //Подключение модулей
-const express = require('express');
-const fs = require('fs');
-const path = require ('path');
-const pug = require('pug');
-const url = require('url')
-const removeFs = require('fs-extra')
-//init app
-const app = express();
+  const express = require('express');
+  const fs = require('fs');
+  const path = require('path');
+  const pug = require('pug');
+  const url = require('url')
+  const removeFs = require('fs-extra')
+  //init app
+  const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');    // указываем используемый шаблонизатор HTML кода
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug'); // указываем используемый шаблонизатор HTML кода
 
-const directory = '/home/smedov/Work/Test/';    //Указываем путь текущей дериктории
+  const directory = '/home/smedov/Work/Node_js/Test/'; //Указываем путь текущей дериктории
 
-let userList = [
-    { id: 1, name: 'Admin', login: 'Admin', password:"qwe"},
-    { id: 2, name: 'TestUser', login: 'test', password:"123"},
-    { id: 3, name: 'Serega', login: 'MRG_Serejka', password:"12345"},
-    { id: 4, name: 'Artur', login: 'ARCHI', password:"qwerty"}
-];
-
-
-app.use(express.static(path.join(__dirname, 'public')));    //добовляет файлы которые на компьютере для загрузки если они имеются
-
-//Главная страница
-app.get('/', function (req, res) {//Главная страница
-    const files = fs.readdirSync(directory);           //Прочитываем файлы из текущей директории
-
-    for(let i=0; i<files.length;i++)               //убираем расширение
-      {
-        let name = path.basename(files[i], '.conf');
-        files[i] = name;
-      }
-
-    let searchResult = req.query.search;    //присваиваем переменной результат запроса клиента
+  let userList = [
+      { id: 1, name: 'Admin', login: 'Admin', password:"qwe"},
+      { id: 2, name: 'Igor', login: 'Amstel', password:"123"},
+      { id: 3, name: 'Serega', login: 'MRG_Serejka', password:"12345"},
+      { id: 4, name: 'Artur', login: 'ARCHI', password:"qwerty"}
+  ];
 
 
-    const filterItems = (searchResult) => {             //фильтр Поискового окна
+  app.use(express.static(path.join(__dirname, 'public'))); //добовляет файлы которые на компьютере для загрузки если они имеются
+
+  //Главная страница
+  app.get('/', function(req, res) { //Главная страница
+    const files = fs.readdirSync(directory); //Прочитываем файлы из текущей директории
+
+    for (let i = 0; i < files.length; i++) //убираем расширение
+    {
+      let name = path.basename(files[i], '.conf');
+      files[i] = name;
+    }
+
+    let searchResult = req.query.search; //присваиваем переменной результат запроса клиента
+
+
+    const filterItems = (searchResult) => { //фильтр Поискового окна
       return files.filter((el) =>
         el.indexOf(searchResult) > -1
       );
     }
 
-    let filterList = filterItems(searchResult);             //присваиваем отфильтрованные переменные в одну переменную
+    let filterList = filterItems(searchResult); //присваиваем отфильтрованные переменные в одну переменную
 
 
-    if(filterList!=''){
-      res.render('index', { title: 'Directory', value: filterList});
-    }else
-
-    res.render('index', { title: 'Directory', value: files});       //рендерим файл index.pug
+    if (filterList != '') {
+      res.render('index', {
+        title: 'Directory',
+        value: filterList
       });
+    } else
 
-
-    app.get('/delete', function(req, res) {          //  удаления файла из текущей директории
-        const folder = directory+req.query.id+'.conf';
-        console.log(folder)
-        removeFs.remove(folder, err => {
-          if(err) console.error(err),
-          res.send("Файл " + req.query.id + " был успешно удален");
-        })
-
-
-    });
-
-
-
-
-
-  app.get('/add', function(req, res){     //добавление
-
-      let domain = req.query.domain;
-      let fileName = directory + domain + '.conf'
-      let ip = req.query.ip;
-      let domenWithoutDots=domain.replace(/\./g, ""); //убираем точку глабально
-
-
-      let fileContent = fs.readFileSync('/home/smedov/Work/Test/template.conf', "utf8");
-      var newStr = fileContent.replace(/__DOMAINWITHOUTDOT__/g, domenWithoutDots).replace(/__DOMAIN__/g, domain).replace(/__IP_ADDRESS__/g,ip);
-
-      //записываем в файл домен и ip
-      fs.writeFile(fileName, newStr , function(error){
-        if(error) throw error; //Использую инструкцию throw для генерирования исключения
-
-
-        res.send("200");//выведем 200ок
-
-      });
+      res.render('index', {
+        title: 'Directory',
+        value: files
+      }); //рендерим файл index.pug
   });
 
-app.get('/login', function(req, res){     //авторизация
-    res.render('login', {title:'Вход'});
-});
 
-function check(userLogin)
-  {
-  for (let i=0 ; i< userList.length;i++)
-      {
-          if (userList[i].login === userLogin)
-              {
+  app.get('/delete', function(req, res) { //  удаления файла из текущей директории
+    const folder = directory + req.query.id + '.conf';
+    console.log(folder)
+    removeFs.remove(folder, err => {
+      if (err) console.error(err),
+        res.send("Файл " + req.query.id + " был успешно удален");
+    })
 
-              console.log(userList[i]);
-              return userList[i];
-              }
+
+  });
+
+
+
+
+
+  app.get('/add', function(req, res) { //добавление
+
+    let domain = req.query.domain;
+    let fileName = directory + domain + '.conf'
+    let ip = req.query.ip;
+    let domenWithoutDots = domain.replace(/\./g, ""); //убираем точку глабально
+
+
+    let fileContent = fs.readFileSync('/home/smedov/Work/Node_js/Test/template.conf', "utf8");
+    var newStr = fileContent.replace(/__DOMAINWITHOUTDOT__/g, domenWithoutDots).replace(/__DOMAIN__/g, domain).replace(/__IP_ADDRESS__/g, ip);
+
+    //записываем в файл домен и ip
+    fs.writeFile(fileName, newStr, function(error) {
+      if (error) throw error; //Использую инструкцию throw для генерирования исключения
+
+
+      res.send("200"); //выведем 200ок
+
+    });
+  });
+
+  app.get('/login', function(req, res) { //авторизация
+    res.render('login', {
+      title: 'Вход'
+    });
+  });
+
+  function check(userLogin) {
+    for (let i = 0; i < userList.length; i++) {
+      if (userList[i].login === userLogin) {
+
+        console.log(userList[i]);
+        return userList[i];
       }
+    }
 
-  return false;
+    return false;
   }
 
 
-  app.get('/getLogin', function (req, res) {    //авторизация
-      let login = req.query.login
-      let password = req.query.password
-      const uniqueUser = check(login)
+  app.get('/getLogin', function(req, res) { //авторизация
+    let login = req.query.login
+    let password = req.query.password
+    const uniqueUser = check(login)
 
-      if(password === uniqueUser.password){
+    if (password === uniqueUser.password) {
 
-            const out = {
-                status: 1,
-                token: 'supertoken-3213123123',
-                str:req.query.login+'--'+req.query.password,
-                name: req.query.login,
-                user: uniqueUser.id
-            }
-          res.json(out)            //отправляю json формат на клиент
-        }
-        else{
-      res.json({status:0})
-    }
-   });
-
-   app.get('/admin',function(req, res){
-     res.render('admin', {title:'Админка', userList:userList});
-   });
-
-   app.get('/ajax/admin', function(req, res){     //авторизация под админа
-      let newUserLogin = req.query.login;
-      let newUserName = req.query.name;
-      let newUserPassword = req.query.password;
-      console.log(newUserLogin)
-      let user={
-        id:userList.length+1,
-        login : newUserLogin,
-        name : newUserName,
-        password : newUserPassword
+      const out = {
+        status: 1,
+        token: 'supertoken-3213123123',
+        str: req.query.login + '--' + req.query.password,
+        name: req.query.login,
+        user: uniqueUser.id
       }
-      userList.push(user)
-      res.send("200");
-   });
+      res.json(out) //отправляю json формат на клиент
+    } else {
+      res.json({
+        status: 0
+      })
+    }
+  });
+
+  app.get('/admin', function(req, res) {
+    res.render('admin', {
+      title: 'Админка',
+      userList: userList
+    });
+  });
+
+  app.get('/ajax/admin', function(req, res) { //авторизация под админа
+    let newUserLogin = req.query.login;
+    let newUserName = req.query.name;
+    let newUserPassword = req.query.password;
+
+    let resultAunth = authentication(userList,newUserLogin)
+    if (resultAunth == false){
+    let user = {
+      id: userList.length + 1,
+      login: newUserLogin,
+      name: newUserName,
+      password: newUserPassword,
+
+    }
+    else {
+      alert("Логин занят")
+    }
+
+  }
+    userList.push(user)
+    res.send("200");
+  });
 
 
 
 
-//запускаем сервер
-app.listen(3000, function () {
-    console.log ('Отслеживаем порт: 3000!');
-});
+  //запускаем сервер
+  app.listen(3000, function() {
+    console.log('Отслеживаем порт: 3000!');
+  });
