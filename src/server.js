8e9d752e35de353fef4082e87e5e210236a250da
Ваше Имя <you@example.com>
2019-09-12 12:25:14 +0300

    axios.defaults.headers.common['Authorization'] = AUTH_TOKEN

--- src/server.js
+++ src/server.js
@@ -1,26 +1,26 @@
   //Подключение модулей
-  const express = require('express');
-  const path = require('path');
-  const pug = require('pug');
-  const cors = require('cors'); //модуль для axios
+  const express = require('express');         // подключаем express
+  const path = require('path');               //модуль path позволяет указывать пути к дирректориям
+  const pug = require('pug');                 // подключаем модуль шаблонизатора
+  const cors = require('cors');               //модуль для
   const bodyParser = require('body-parser');
-  const createError = require('http-errors')
+  const createError = require('http-errors')  // модуль отлавливания ошибок
   const jwt = require('jsonwebtoken');
 
-  //init app
-  const app = express();
+  const app = express(); //init app
 
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'pug'); // указываем используемый шаблонизатор HTML кода
+  app.set('views', path.join(__dirname, 'views')); //указываем путь к pug файлам
+  app.set('view engine', 'pug');                  // указываем используемый шаблонизатор HTML кода
 
   app.use(bodyParser.urlencoded({ extended: false }))
   app.use(bodyParser.json())
+
   app.use(cors());
+
   app.use(express.static(path.join(__dirname, 'public'))); //добовляет файлы которые на компьютере для загрузки если они имеются
 
-  const MY_SECRET = "cAtwa1kkEy"
-
-  let userList = [
+  const MY_SECRET = "cAtwa1kkEy"      // случайный секретный ключ
+  let userList = [                    // массив пользователей
       { id: 1, name: 'Admin', login: 'Admin', password:"qwe"},
       { id: 2, name: 'Igor', login: 'Amstel', password:"123"},
       { id: 3, name: 'Serega', login: 'MRG_Serejka', password:"12345"},
@@ -31,11 +31,11 @@
       { id: 8, name: 'Irina', login: 'Beller', password:"qwerty"}
   ];
 
-  let lengthArray = userList.length // переменная хранящая длинну массива
+  let lengthArray = userList.length   // переменная хранящая длинну массива
 
-    // ФУНКЦИИ Вспомогательные
+    // ФУНКЦИИ Вспомогательные__________________________________________________
 
-    function searchById (userList, id) {
+    function searchById(userList, id) {
       for (let i = 0; i < userList.length; i++) {
         if (userList[i].id == id) {
           return userList[i]
@@ -44,7 +44,7 @@
       return false
     };
 
-    function loginСomparison (userList, login) {
+    function loginСomparison(userList, login) {
       for (let i = 0; i < userList.length; i++) {
         if (userList[i].login === login) {
           return userList[i]
@@ -53,67 +53,45 @@
       return false
     };
 
-    function passwordСheck (userList, password) {
-      for (let i = 0; i < userList.length; i++) {
-        if (userList[i].password === password) {
-          return userList[i]
-        }
-      }
-      return false
-    };
-//ФУНКЦИИ КОТОРЫМ НЕ НУЖЕН ТОКЕН ДЛЯ ВЫПОЛНЕНИЯ____________________________________________
+//ФУНКЦИИ КОТОРЫМ НЕ НУЖЕН ТОКЕН ДЛЯ ВЫПОЛНЕНИЯ_________________________________
+app.post('/ajax/users/dataChecking', function(req, res, next) {
+  let userLogin = req.body.login; //name пользователя
+  let userPassword = req.body.password; //password пользователя
+  let checkUser = loginСomparison(userList, userLogin) //проверим есть ли такой пользоваль
 
-    app.post('/ajax/users/dataChecking', function(req, res,next) {
-      let userLogin = req.body.login; //name пользователя
-      let userPassword = req.body.password; //password пользователя
+  if (checkUser && checkUser.password === userPassword) {
+    let user = loginСomparison(userList, userLogin) //получаем Объект пользователя
+    let token = jwt.sign({ name: user.id, login: user.login }, MY_SECRET); //хешируем токен используя секретный ключ
 
-      if (loginСomparison(userList, userLogin) && passwordСheck(userList, userPassword)) {
-        let user = loginСomparison(userList, userLogin) //получаем Объект пользователя
-        let token = jwt.sign({ name: user.id, login: user.login }, MY_SECRET); //хещируем токен используя секретный ключ
-
-        res.json({  //отправим ответ на сервер JSON
-          token: token, // захешированный токен
-          userId: user.id,
-          userName: user.name,
-          userLogin: user.login
-        })
-      }else {
-        return next(createError(400, 'Вы ввели неправильные логин или пароль'))
-      }
+    res.json({ //отправим ответ на сервер JSON
+      token: token, // захешированный токен
+      userId: user.id,
+      userName: user.name,
+      userLogin: user.login
     })
+  } else {
+    return next(createError(400, 'Вы ввели неправильные логин или пароль'))
+  }
+})
 
-//ОБРАБОТЧИК ПЕРЕХВАТЫВАЕТ ВСЕ ПУТИ________________________
-
-
+//ОБРАБОТЧИК ПЕРЕХВАТЫВАЕТ ВСЕ ПУТИ_____________________________________________
 
 app.use('*', function(req, res, next) {
+  let token       //?????
+  let result = (req.headers.authorization)
+  if(result) token = result.substr(7)
+  console.log(token)
 
-  let token = req.query.token
-  if(!token) token = req.body.token
-
-
-  //
-  //let token = req.query.token ?  req.query.token : req.body.token
-  //
-  //let token = req.query.token || req.body.token
-  //
-
-
-
-
-
-  console.log("token: " ,token)
   if (!token) { // приводим к булевному значению (то что токена не существует)
     return next(createError(412, 'Токен не сушествует'))
   }
   let decoded = jwt.verify(token, MY_SECRET); // расшифруем токен
-
   if (!decoded) {
     return next(createError(416, 'Токен не валиден'))
   } else next()
 })
 
-//ФУНКЦИИ ДЛЯ КОТОРЫХ НУЖЕН ТОКЕН__________________________________________________________________________________
+//ФУНКЦИИ ДЛЯ КОТОРЫХ НУЖЕН ТОКЕН_______________________________________________
 
 app.get('/ajax/users', function(req, res,next) {
   res.json(userList) // рендерим массив пользователей
@@ -124,7 +102,7 @@ app.post('/ajax/users/delete', function(req, res,next) {    // удаление 
   let uniqueUserId = Number(req.body.id) // Id пользователя
   let resultRemoveUser = searchById(userList, uniqueUserId) // функция аунтификации по id
 
-  if (Boolean(resultRemoveUser)) {
+  if (resultRemoveUser) {
     let userIndexReal = userList.indexOf(resultRemoveUser);
     userList.splice(userIndexReal, 1);
     res.json(userList)
@@ -155,19 +133,14 @@ app.post('/ajax/users/add', function(req, res, next) {
   }
 });
 
+//ОТЛАВЛИВАЕМ ОШИБКИ ЗДЕСЬ
+//Используется модуль http-errors_______________________________________________
 
-    //ОТЛАВЛИВАЕМ ОШИБКИ ЗДЕСЬ
-    //Используется модуль http-errors
-
-app.use(function(req, res, next) {
+app.use(function(req, res, next) { //ОТЛАВЛИВАЕМ ВСЕ НЕ СУЩЕСТВУЕЩИЕ ПУТИ И ВЫВЕДЕМ СООТВЕТСВУЮЩУЮ ОШИБКУ
   return next(createError(404, 'Api метод не существует'))
 })
 
-app.use(function(err, req, res, next) {
-/*  console.log(err.statusCode)
-  console.log(err)
-  console.log(err.__proto__)
-  */
+app.use(function(err, req, res, next) { //ВЫВЕДЕМ В NETWORK ОТФАРМАТИРОВАННОЕ СООЬЩЕНИЕ ОБ ОШИБКЕ
   res.status(err.statusCode || 500)
   res.json({
     success: 0,
@@ -175,7 +148,6 @@ app.use(function(err, req, res, next) {
     message: err.message
   })
 })
-
 
 //запускаем сервер
 app.listen(3000, function() {
